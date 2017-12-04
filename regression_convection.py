c7824dcdf2c49a903d150bff1e2d0be4e4af020a
Renne15 <renne00.xv@gmail.com>
2017-12-05 02:06:48 +0900

    Update : convection-diffusion.py

--- regression_convection.py
+++ regression_convection.py
@@ -19,9 +19,8 @@ def zscore(x, axis = None):
     return zscore
 
 #データ間の距離
-dt = 0.0006250000000000001*10 #sec
-dx = 0.1 #m
-dy = 0.1 #m
+dt = 0.0006250000000000001 *10 #sec
+dx = dy = 0.1 #m
 
 #データの勾配
 f_t = np.gradient(scalar_fields, axis=0)/dt
@@ -37,45 +36,30 @@ f_x_col = f_x.flatten()
 f_y_col = f_y.flatten()
 f_xx_col = f_xx.flatten()
 f_yy_col = f_yy.flatten()
-partial = np.c_[f_xx_col, f_yy_col, f, f_x_col, f_y_col]
 partial = np.c_[f_xx_col, f_yy_col, f_y_col]
-
-#回帰分析できる形に変形
-# f = scalar_fields[:,40:60,50:60].flatten()
-# f_t_col = f_t[:,40:60,50:60].flatten()
-# f_x_col = f_x[:,40:60,50:60].flatten()
-# f_y_col = f_y[:,40:60,50:60].flatten()
-# f_xx_col = f_xx[:,40:60,50:60].flatten()
-# f_yy_col = f_yy[:,40:60,50:60].flatten()
-# partial = np.c_[f_xx_col, f_yy_col, f, f_x_col, f_y_col]
-# partial = np.c_[f_xx_col, f_yy_col, f_y_col]
 
 ###変化のない部分と外れ値をdeleteする
 partial = np.delete(partial, np.where(np.absolute(f_t_col)<1) ,0)
 f_t_col = np.delete(f_t_col, np.where(np.absolute(f_t_col)<1))
 
-plt.scatter(f_t_col,4*(partial[:,0]+partial[:,1])-10*partial[:,2]-f_t_col, s=1, c='purple', marker='s', label='Residual error')
-plt.xlabel('f_t')
-plt.ylabel('Residual error')
-plt.savefig("./data/reg_con0.png")
-plt.show()
+# plt.scatter(f_t_col,4*(partial[:,0]+partial[:,1])-10*partial[:,2]-f_t_col, s=1, c='purple', marker='s', label='Residual error')
+# plt.xlabel('f_t')
+# plt.ylabel('Residual error')
+# plt.savefig("./data/reg_con0.png")
+# plt.show()
 
 partial = np.delete(partial, np.where(np.absolute(f_t_col)>1000) ,0)
 f_t_col = np.delete(f_t_col, np.where(np.absolute(f_t_col)>1000))
 
 #配列サイズ確認
-# print('f',f.shape)
-# print('f_t_col',f_t_col.shape)
-# print('f_x_col',f_x_col.shape)
-# print('f_y_col',f_y_col.shape)
-# print('f_xx_col',f_xx_col.shape)
-# print('f_yy_col',f_yy_col.shape)
-# print('partial',partial.shape)
+print('f_t_col',f_t_col.shape)
+print('partial',partial.shape)
 
 #標準化
 # partial = zscore(partial, axis=0)
 # f_t_col = zscore(f_t_col, axis=0)
 
+#目的関数の平均値
 print('f_t_col ave:',np.average(f_t_col))
 print('|f_t_col| ave:',np.average(np.absolute(f_t_col)))
 
@@ -83,16 +67,16 @@ print('|f_t_col| ave:',np.average(np.absolute(f_t_col)))
 reg.fit(partial,f_t_col)
 
 #回帰係数
-print(pd.DataFrame({"Name":['f_xx', 'f_yy','f_y'],# 'f', 'f_x', 'f_y'],
+print(pd.DataFrame({"Name":['f_xx', 'f_yy','f_y'],
                     "Coefficients":reg.coef_}).sort_values(by='Coefficients'))
 print("切片",reg.intercept_)
 print("R2",reg.score(partial, f_t_col))
 
 ###残差プロット
-X = reg.predict(partial)
-plt.scatter(f_t_col, X - f_t_col, s=10, c='purple', marker='s', label='Residual error')
-plt.hlines(y=0, xmin=-1000, xmax=1000, lw=2, color='red')
-plt.xlabel('f_t')
-plt.ylabel('Residual error')
-plt.savefig("./data/reg_con.png")
-plt.show()
+# X = reg.predict(partial)
+# plt.scatter(f_t_col, X - f_t_col, s=10, c='purple', marker='s', label='Residual error')
+# plt.hlines(y=0, xmin=-1000, xmax=1000, lw=2, color='red')
+# plt.xlabel('f_t')
+# plt.ylabel('Residual error')
+# plt.savefig("./data/reg_con.png")
+# plt.show()
