ea316224569f467c0d491aa99cc83fd233c8bfa0
Renne15 <renne00.xv@gmail.com>
2017-12-16 06:08:37 +0900

    Update

--- generate_1D.py
+++ generate_1D.py
@@ -6,8 +6,10 @@ from math import exp, erfc, sqrt
 c = 1.
 D = 5.
 
-t_size = 100
-x_size = 100
+x_split = 0.1
+t_split = 0.1
+t_size = 5000
+x_size = 5000
 phi = np.zeros((t_size,x_size))
 
 for t in range(t_size):
@@ -17,15 +19,16 @@ for t in range(t_size):
         elif x == 0:
             phi[t,x] = 1.
         else:
-            phi[t,x] = 0.5 * exp(x*c/(2*D)) * ( exp( -x*c/(2*D) ) * erfc( (x-c*t)/(2*sqrt(D*t)) )
-                                            + exp( x*c/(2*D) ) * erfc( (x+c*t)/(2*sqrt(D*t)) ) )
+            phi[t,x] = 0.5 * np.exp(x*x_split*c/(2*D)) * ( np.exp( -x*x_split*c/(2*D) ) * erfc( (x*x_split-c*t*t_split)/(2*sqrt(D*t*t_split)) )
+                                            + np.exp( x*x_split*c/(2*D) ) * erfc( (x*x_split+c*t*t_split)/(2*sqrt(D*t*t_split)) ) )
 
-for t in range(t_size):
-    plt.plot(range(x_size),phi[t,:])
-    plt.ylim([0,1])
-    plt.draw()
-    plt.pause(0.01)
-    plt.clf()
+# ### 移流拡散のプロット
+# for t in range(t_size):
+#     plt.plot( [x_split*x for x in range(x_size) ], phi[t,:] )
+#     plt.ylim([0,1])
+#     plt.draw()
+#     plt.pause(0.001)
+#     plt.clf()
 
 filepath = './data/data_1D.csv'
 np.savetxt(filepath, phi, delimiter=',')
